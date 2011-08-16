43cf3e892eaaf8cd80416423c9cca930f7246f18
Tom Kent <teeks99@yahoo.com>
2011-08-16 10:19:07 -0400

    Broke out into functions.

--- SpeedCheck.py
+++ SpeedCheck.py
@@ -7,46 +7,78 @@ Created on Aug 15, 2011
 import urllib2
 import time
 
-if __name__ == '__main__':
-    url = "http://speedtest.wdc01.softlayer.com/downloads/test500.zip"
-    seconds = 60
-    block_size = 1024
-    output_time = 5
+def push_block_size(remote_file, block_size, block_window=1.0, telemetry=False):
+    """ Grow the blocksize to a suitable value for this connection
     
-    f = urllib2.urlopen(url)
-    
+    If the block is downloaded in less than block_window seconds, then double it. 
+    Repeat until we have two runs in a row that take longer than block_window
+    """
+    start = time.time()
+    previous = start
+    last = False
+    second = False
+    while not last and not second:
+        temp = f.read(block_size)
+        if len(temp)==0: #We've gotten all the file
+            raise IOError("file was to short to complete setup")
+        current = time.time()
+        if(current-previous) < block_window:
+            block_size = block_size * 2
+            if telemetry:
+                print "doubling block size to: " + str(block_size) + "B"
+            last = False
+            second = False
+        elif last:
+            second = True
+        else: 
+            last = True
+        previous = current
+    return block_size
+
+def speed_check(remote_file, test_seconds, block_size, output_time=0):
+    average, bytes, elapsed, block_size = speed_check_info(remote_file,  
+        test_seconds, block_size, output_time)
+    return average
+
+def speed_check_info(remote_file, test_seconds, block_size, output_time=0):
     start = time.time()
     bytes = 0
-    previous = 0
+    previous = start
     last_output = start
+
     while previous < (start + seconds):
         temp = f.read(block_size)
-        if len(temp)==0: 
+        if len(temp)==0: #We've gotten all the file
             break
         bytes += block_size
         current = time.time()
-        
-        # We want to push the block size up as high as we can, to
-        # avoid unknowns surrounding what happens at the start/stop of 
-        # a read() call.
-        if (current - previous) < 0.5:
-            block_size = block_size * 2
-            #print "doubled to: " + str(block_size)
             
-        if (current - last_output) > output_time:
-            #print str(bytes/(current-start))
+        if (current - last_output) > output_time and output_time != 0:
+            print str(bytes/(current-start))
             last_output = current
             
         previous = current
         
     finish = previous
-    f.close()
     elapsed = finish-start
-    
     average = bytes / elapsed
-    #print "done: " + str(average)
+   
+    return average, bytes, elapsed, block_size
+
+if __name__ == '__main__':
+    url = "http://speedtest.wdc01.softlayer.com/downloads/test500.zip"
+    seconds = 60
+    block_size = 1024
+    output_time = 5
+    block_window = 0.5
+    append_file = 'log.txt'
     
-    out = open('log.txt','a')
+    f = urllib2.urlopen(url)
+    block_size = push_block_size(f, block_size, block_window)
+    average = speed_check(url, seconds, block_size)
+    f.close()
+     
+    out = open(append_file,'a')
     out.write(str(average)+'\n')
     out.close()
     
