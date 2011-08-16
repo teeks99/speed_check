a607efb382afc61b0722f846037d442e4e77ec3e
Tom Kent <teeks99@yahoo.com>
2011-08-16 17:30:15 -0400

    new parser for options (compatible with older pythons)

--- SpeedCheck.py
+++ SpeedCheck.py
@@ -7,6 +7,7 @@ Created on Aug 15, 2011
 import urllib2
 import time
 import argparse
+import optparse
 import monotonic
 
 def push_block_size(remote_file, block_size, block_window=1.0, telemetry=False):
@@ -15,15 +16,16 @@ def push_block_size(remote_file, block_size, block_window=1.0, telemetry=False):
     If the block is downloaded in less than block_window seconds, then double it. 
     Repeat until we have two runs in a row that take longer than block_window
     """
+    #prime the pump
+    temp = f.read(block_size)
+    temp = f.read(block_size)
+    temp = f.read(block_size)
+
     start = monotonic.monotonic_time()
     previous = start
     last = False
     second = False
     debug_data = []
-    #prime the pump
-    temp = f.read(block_size)
-    temp = f.read(block_size)
-    temp = f.read(block_size)
     while not last and not second:
         temp = f.read(block_size)
         if len(temp)==0: #We've gotten all the file
@@ -45,8 +47,10 @@ def push_block_size(remote_file, block_size, block_window=1.0, telemetry=False):
             last = False
             second = False
         elif last:
+            #TODO: if elapsed time is more than a few percent over block_window, lower block_size
             second = True
         else: 
+            #TODO: if elapsed time is more than a few percent over block_window, lower block_size
             last = True
         previous = current
     debug_data.append((current-previous, block_size))
@@ -100,9 +104,24 @@ if __name__ == '__main__':
     parser.add_argument('-a','--append-file',help="File to append this result")
     parser.add_argument('-r','--no-result',dest="print_result",help="Don't print the result, handy for cron with -a",action="store_false")
     parser.add_argument('-g','--growth', help="Show block size growth",action="store_true")
-    parser.add_argument
-    args = parser.parse_args()
-    #print args.url, args.time, args.message_freq, args.block, args.window, args.append_file, args.no_result, args.growth
+    #args = parser.parse_args()
+    parser = optparse.OptionParser()
+    parser.add_option('-u','--url', help="URL of the file to try downloading"+
+        "defaults to http://speedtest.wdc01.softlayer.com/downloads/test500.zip",
+        default="http://speedtest.wdc01.softlayer.com/downloads/test500.zip")
+    parser.add_option('-t','--time', help="Number of seconds to run the test, default 60",
+        default=60, type=float)
+    parser.add_option('-m','--message-freq', help="Output message frequency in seconds",
+        default=0, type=float)
+    parser.add_option('-b','--block',help="Initial block size",default=1024, 
+        type=int)
+    parser.add_option('-w','--window',help="Block window lenght, default 0.5s", 
+        default=0.5, type=float)
+    parser.add_option('-a','--append-file',help="File to append this result")
+    parser.add_option('-r','--no-result',dest="print_result",help="Don't print the result, handy for cron with -a",action="store_false",default=True)
+    parser.add_option('-g','--growth', help="Show block size growth",action="store_true",default=False)
+    (args, additional_args) = parser.parse_args()
+    print args.url, args.time, args.message_freq, args.block, args.window, args.append_file, args.print_result, args.growth
     
     f = urllib2.urlopen(args.url)
     block_size = push_block_size(f, args.block, args.window, args.growth)
